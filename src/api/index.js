c478f5cd707965ceda4583b14e0d56482653dca9
Khadim <khadimfall0602@gmail.com>
2021-04-26 22:29:09 +0200

    added basic auth

--- src/api/index.js
+++ src/api/index.js
@@ -3,6 +3,9 @@ module.exports = {
         {
             method: 'GET',
             path: '/',
+            options: {
+                auth: false
+            },
             handler: (request, h) => {
                 return '<a href="https://matchnlearn.com">MatchNLearn</a> WA API';
             }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 require('dotenv').config()
 const Hapi = require('@hapi/hapi');
 const { registerRoutes } = require('./routes');
+const { validateGenerator } = require('./auth');
 const db = require('./db');
 const DBDataPersistence = require('./services/DBDataPersistence');
 const WhatsAppService = require('./services/WhatsAppService');
 
 // const IS_DEVELOP = process.env.NODE_ENV === "development"; 
+const adminUser = {
+    username: "admin",
+    password: process.env.ADMIN_PASSWORD
+};
 
 const init = async () => {
     const server = Hapi.server({
@@ -19,7 +24,11 @@ const init = async () => {
     await db.init(process.env.MONGO_URL);
     await DBDataPersistence.pullFiles();
     WhatsAppService.init();
+
+    await server.register(require('@hapi/basic'));
     registerRoutes(server);
+    server.auth.strategy('simple', 'basic', { validate: validateGenerator([adminUser]) });
+    server.auth.default('simple');
 
     await server.start();
     console.log('Server running on %s', server.info.uri);
